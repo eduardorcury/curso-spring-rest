dc39181d47328957f3d6feafde7df1d7254193d7
eduardorcury <eduardoribeirocury@gmail.com>
2020-06-17 12:06:43 -0400

    Commit inicial

---
+++ src/main/java/com/eduardorib/api/controller/ClienteController.java
@@ -0,0 +1,23 @@
+package com.eduardorib.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.eduardorib.domain.model.Cliente;
+import com.eduardorib.domain.repository.ClienteRepository;
+
+@RestController
+public class ClienteController {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@GetMapping("/clientes")
+	public List<Cliente> listar() {
+		return clienteRepository.findAll();
+	}
+
+}
